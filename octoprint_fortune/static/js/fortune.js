f2d7c4ed2d77cfcb90cdcc05b8f60f560d48706d
Garry Plays <31503736+GarryPlays@users.noreply.github.com>
2021-06-06 11:41:03 +0200

    adding timeout calculator

--- octoprint_fortune/static/js/fortune.js
+++ octoprint_fortune/static/js/fortune.js
@@ -36,7 +36,9 @@ $(function() {
                         new PNotify({
                             title: gettext("Your Fortune!"),
                             text: gettext(response.data),
-                            type: "success"
+                            type: "success",
+                            // adding both string lengths, parseInt in case something wonky is in response, 55ms per letter read time, additional 100ms as reaction speed to notice the popup and move your eyes there
+                            delay: parseInt(gettext("Your Fortune!").length + gettext(response.data).length) * 55 + 100
                         });
                     } else {
                         var text;
